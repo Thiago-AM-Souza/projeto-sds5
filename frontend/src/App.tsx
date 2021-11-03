235e9e7d807c05d2dac01ca2fd2fd2866acf0197
Thiago-AM-Souza <thiagosouza96.ts@gmail.com>
2021-11-03 14:43:12 -0300

    Security config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,14 +17,11 @@ function App() {
             <h5 className="text-center text-secondary">Taxa de sucesso</h5>
             <BarChart />
           </div>
-        </div>
-        <div className="row px-3">
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">Todas vendas</h5>
             <DonutChart />
           </div>
         </div>
-
         <div className="py-3">
           <h2 className="text-primary">Todas vendas</h2>
         </div>
